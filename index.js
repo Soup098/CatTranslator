0805ecd091e4a2660c4b326ccaf1357bf1d867ac
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2024-08-07 23:19:11 -0600

    changes
    added some more phrases

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const translations = ["Meow","FEED ME!","Silly hunam, sit down now. All your laps are belong to us.","I find your lack of pets disturbing.", "I require sustenance", "How dare you disturb my slumber!","Oh hello, my servant has arrived.", "I am cat, hear me ROAR... Meow", "The squirrels and the birds mock me. One day I will have my revenge!", "You smell of other cats... how could you?", "I will have my treat NOW" ]
+const translations = ["Meow","FEED ME!","Silly hunam, sit down now. All your laps are belong to us.","I find your lack of pets disturbing.", "I require sustenance", "How dare you disturb my slumber!","Oh hello, my servant has arrived.", "I am cat, hear me ROAR... Meow", "The squirrels and the birds mock me. One day I will have my revenge!", "You smell of other cats... how could you?", "I will have my treat NOW", "meow? MEOW? you're not looking at the big picture!", "Good taste really isnt that easy to recognize", "Hello, pet human" ]
 
 const translate = document.getElementById("translate")
 const translation = document.getElementById("translation")
